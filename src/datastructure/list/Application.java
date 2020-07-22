4f197e89fb4ad026307bc8a35edaeda0cfccf2c6
Arun <arunkannan1935@gmail.com>
2020-07-22 23:39:43 +0530

    cleaned code

--- src/datastructure/list/Application.java
+++ src/datastructure/list/Application.java
@@ -1,10 +1,7 @@
 package datastructure.list;
 
 public class Application {
-    public static void main(String args[]){
-//        java.util.LinkedList linkedList = new java.util.LinkedList();
-
-
+    public static void main(String args[]) {
         LinkedList list = new LinkedList();
         list.add(12);
         list.add(15);
